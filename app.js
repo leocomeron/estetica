03c84ef52fb39c6ed8355876f3234d306620ebfd
lcomeron <leocomeron@gmail.com>
2021-09-11 23:42:04 -0300

    2da modificacion

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
+const come = "leandro"
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
