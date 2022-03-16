a3c3516579c13efad22ab2132a1cd2c8ca20eafa
Leandro Comeron <89902573+leocomeron@users.noreply.github.com>
2022-03-16 12:36:49 -0300

    Cleaning the code

---
+++ app.js
@@ -0,0 +1,74 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
+const methodOverride = require("method-override");
+
+const app = express();
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+
+const secret = process.env.SECRET || "thisshouldbeabettersecret!";
+
+app.get("/inicio", (req, res) => {
+  res.render("index");
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/dra-Renee", (req, res) => {
+  res.render("dra-Renee");
+});
+app.get("/aparatologia", (req, res) => {
+  res.render("aparatologia");
+});
+app.get("/tratamientos", (req, res) => {
+  res.render("tratamientos");
+});
+app.get("/depilacion", (req, res) => {
+  res.render("depilacion");
+});
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Oh No, Something Went Wrong!";
+  res.status(statusCode).render("error", { err });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`);
+});
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
